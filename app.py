35ed22f37c0ef9e15dbc4eb9fae36f2c67005c12
RITA CHAMBAZ <chambazritamarie@gmail.com>
2025-12-17 19:51:50 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
